9877a58e1a17d1cf94042f2334c5ef6bedd03acf
Ankur <ankurshahi@Ankurs-iMac.phub.net.cable.rogers.com>
2021-07-04 18:29:59 -0400

    added Get notes api route to return all saved notes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const notes = require('./db/db.json');
 const app = express();
 const path = require('path');
+
 const PORT = process.env.PORT || 3001
 
 // Middleware
 app.use(express.static('public'));
+
+// get notes
+app.get('/api/notes',(req,res)=>{
+    res.json(notes);
+});
 
 // Get notes html page
 app.get('/notes',(req,res)=>{
@@ -15,6 +22,7 @@ app.get('/notes',(req,res)=>{
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'./public/index.html'));
 });
+
 app.listen(PORT,()=>{
     console.log(`API server now on port 3001`);
 })
