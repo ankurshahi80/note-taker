eb8fd7dab6130a479e001848814e4e0e6873c82a
Ankur <ankurshahi@Ankurs-iMac.phub.net.cable.rogers.com>
2021-07-04 20:27:50 -0400

    API routes and the fucnction to write to the database added

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const notes = require('./db/db.json');
 const app = express();
-const path = require('path');
+const uniqid = require('uniqid');
 
 const PORT = process.env.PORT || 3001
 
 // Middleware
+// Parse incoming string or array data
+app.use(express.urlencoded({extended:true}));
+// parse incoming JSON data
+app.use(express.json());
+
 app.use(express.static('public'));
+
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray,null,2)
+    );
+    return note;
+}
 
 // get notes
 app.get('/api/notes',(req,res)=>{
     res.json(notes);
+});
+
+// add notes
+app.post('/api/notes',(req,res)=>{
+    req.body.id=uniqid();
+
+    const note = createNewNote(req.body,notes);
+
+    res.json(note);
 });
 
 // Get notes html page
